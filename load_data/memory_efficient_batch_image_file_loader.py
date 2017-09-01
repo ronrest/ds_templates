72724ea3026eb632b74876a6e66ed1e97ef9a525
ronrest <ronny.coding@gmail.com>
2017-09-02 02:00:31 +1000

    DOCS: Add docstring for create_image_batch_generator_function()

--- load_data/memory_efficient_batch_image_file_loader.py
+++ load_data/memory_efficient_batch_image_file_loader.py
@@ -11,7 +11,41 @@ import glob
 import os
 
 
+# ==============================================================================
+#                                          CREATE_IMAGE_BATCH_GENERATOR_FUNCTION
+# ==============================================================================
 def create_image_batch_generator_function(data_dir, img_shape, pattern="*.jpg", shuffle=True):
+    """
+    Creates a function that iteratively loads images from a directory in
+    batches using a memory efficient *generator*. Ie, the images will
+    only be loaded when needed so you do not need to store all the
+    data on file at once.
+
+    Args:
+    - data_dir:  (str)
+                 Path to the directory housing all the images.
+    - img_shape: (tuple of integers)
+                  What size should the images be resized to during
+                  preprocessing.
+    - pattern:    (str)(default="*.jpg")
+                  Regex pattern to match only files in the directory that
+                  match this pattern.
+
+    Returns: (function)
+    - Returns a function that has the following API:
+        batch_generator(batch_size)
+
+    Examples:
+    For a directory containing 23 RGB images, loading in batches of 10, and
+    resizing images to 100x100:
+
+        >>> batch_generator = create_image_batch_generator_function(datadir, (100,100))
+        >>> for batch in batch_generator(10):
+        >>>     print(batch.shape)
+        (10, 100, 100, 3)
+        (10, 100, 100, 3)
+        (3, 100, 100, 3)
+    """
 
     def batch_generator(batch_size):
         """ A function that returns a generator object that can be iterated
